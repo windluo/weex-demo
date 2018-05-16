f14f40e7af49603ad4e400416c46cdab2108408d
windluo <luo19302@163.com>
2018-05-16 15:03:49 +0800

    readme

--- README.md
+++ README.md
@@ -1,38 +1,51 @@
-## Commands
+## 准备工作
+**开发app必须的Android/IOS环境一定要安装，不然没法模拟和打包！**
 
-### npm start
+这一点weex的官方文档真是简陋，对于没用过Android/IOS开发环境的非常之不友好。可以参考[react native的官方文档](https://reactnative.cn/docs/0.43/getting-started.html)
 
-Starts the development server for you to preview your weex page on browser.
-You can also scan the QR code using weex playground to preview weex page on native.
+### JDK
 
-### npm run dev
+这个是Android环境必须的前置，下载最新的JDK1.8安装、配好环境变量即可。
 
-Open the code compilation task in watch mode.
+### Android
 
-### npm run ios
+目前只尝试了Windows下的Android，做些说明：Android Studio的安装可以到[中文社区](http://www.androiddevtools.cn/)安装，我安装的是[这个版本]([android-studio-ide-181.4763614-windows.zip](https://dl.google.com/dl/android/studio/ide-zips/3.2.0.13/android-studio-ide-181.4763614-windows.zip?utm_source=androiddevtools&utm_medium=website) )。
 
-(Mac only, requires Xcode)
-Starts the development server and loads your app in an iOS simulator.
+> 安装步骤就照着react native那边的文档来，有的地方说*SDK Tools*这一步的*Android SDK Build-Tools 23.0.1*，weex需要的是23.0.2版本，未验证，两个版本都勾选了。
+>
+> 安装过程会把需要的SDK、模拟器都装上，无需操心。
+>
+> Android Studio装好后，注意**不要升级！！！**会有弹框提示升级到3.*版本，千万不要乱升级！！！
+>
+> 之后的怎么打包生成apk、怎么改图标跟名字，网上一搜一大把。
 
-### npm run android
+装好Android Studio后，Android SDK也已经装到了对应的目录，配置好ANDROID_HOME环境变量即可。
 
-(Requires Android build tools)
-Starts the development server and loads your app on a connected Android device or emulator.
+### weex-toolkit
 
-### npm run pack:ios
+这个是[官方](http://weex.apache.org/cn/guide/)提供的工具，可以生成vue项目
 
-(Mac only, requires Xcode)
-Packaging ios project into ipa package.
+> npm install weex-toolkit -g  // 安装工具
+>
+> weex create awesome-app // 创建项目
 
-### npm run pack:android
+## Weex
 
-(Requires Android build tools)
-Packaging android project into apk package.
+### 常用命令
 
-### npm run pack:web
+> npm start // 启动服务
+>
+> weex platform add android // 添加android代码
+>
+> npm run build // 打包vue项目
+>
+> weex run android //官方说是那么说，我实在没跑出什么结果来，弃用
+>
+> npm run pack:android  // 据说可以打包apk，没跑出来
 
-Packaging html5 project into `web/build` folder.
+### 打包apk
 
-### npm run test
+npm run build后，把dist目录下的js（目前只跑了demo的，只生成了js）手动拷贝到*weex-project\platforms\android\app\src\main\assets*目录下，然后跑起Android Studio的打包，即可生成apk。
 
-Starts the test runner.
+开始见证奇迹吧！
+
--- platforms/android/app/build/generated/source/r/debug/com/weex/app/R.java
+++ platforms/android/app/build/generated/source/r/debug/com/weex/app/R.java
@@ -3038,13 +3038,13 @@ containing a value of this type.
         public static final int abc_textfield_search_material=0x7f020051;
         public static final int abc_vector_test=0x7f020052;
         public static final int avd_hide_password=0x7f020053;
-        public static final int avd_hide_password_1=0x7f02006d;
-        public static final int avd_hide_password_2=0x7f02006e;
-        public static final int avd_hide_password_3=0x7f02006f;
+        public static final int avd_hide_password_1=0x7f02006e;
+        public static final int avd_hide_password_2=0x7f02006f;
+        public static final int avd_hide_password_3=0x7f020070;
         public static final int avd_show_password=0x7f020054;
-        public static final int avd_show_password_1=0x7f020070;
-        public static final int avd_show_password_2=0x7f020071;
-        public static final int avd_show_password_3=0x7f020072;
+        public static final int avd_show_password_1=0x7f020071;
+        public static final int avd_show_password_2=0x7f020072;
+        public static final int avd_show_password_3=0x7f020073;
         public static final int design_bottom_navigation_item_background=0x7f020055;
         public static final int design_fab_background=0x7f020056;
         public static final int design_ic_visibility=0x7f020057;
@@ -3054,21 +3054,22 @@ containing a value of this type.
         public static final int error=0x7f02005b;
         public static final int ic_action_refresh=0x7f02005c;
         public static final int ic_action_scan=0x7f02005d;
-        public static final int navigation_empty_icon=0x7f02005e;
-        public static final int notification_action_background=0x7f02005f;
-        public static final int notification_bg=0x7f020060;
-        public static final int notification_bg_low=0x7f020061;
-        public static final int notification_bg_low_normal=0x7f020062;
-        public static final int notification_bg_low_pressed=0x7f020063;
-        public static final int notification_bg_normal=0x7f020064;
-        public static final int notification_bg_normal_pressed=0x7f020065;
-        public static final int notification_icon_background=0x7f020066;
-        public static final int notification_template_icon_bg=0x7f02006b;
-        public static final int notification_template_icon_low_bg=0x7f02006c;
-        public static final int notification_tile_bg=0x7f020067;
-        public static final int notify_panel_notification_icon_bg=0x7f020068;
-        public static final int tooltip_frame_dark=0x7f020069;
-        public static final int tooltip_frame_light=0x7f02006a;
+        public static final int ic_launcher_background=0x7f02005e;
+        public static final int navigation_empty_icon=0x7f02005f;
+        public static final int notification_action_background=0x7f020060;
+        public static final int notification_bg=0x7f020061;
+        public static final int notification_bg_low=0x7f020062;
+        public static final int notification_bg_low_normal=0x7f020063;
+        public static final int notification_bg_low_pressed=0x7f020064;
+        public static final int notification_bg_normal=0x7f020065;
+        public static final int notification_bg_normal_pressed=0x7f020066;
+        public static final int notification_icon_background=0x7f020067;
+        public static final int notification_template_icon_bg=0x7f02006c;
+        public static final int notification_template_icon_low_bg=0x7f02006d;
+        public static final int notification_tile_bg=0x7f020068;
+        public static final int notify_panel_notification_icon_bg=0x7f020069;
+        public static final int tooltip_frame_dark=0x7f02006a;
+        public static final int tooltip_frame_light=0x7f02006b;
     }
     public static final class id {
         public static final int ALT=0x7f0f0054;
@@ -3352,6 +3353,8 @@ containing a value of this type.
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
+        public static final int ic_launcher_foreground=0x7f030001;
+        public static final int ic_launcher_round=0x7f030002;
     }
     public static final class raw {
         public static final int zxing_beep=0x7f080000;
